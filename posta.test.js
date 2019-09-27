18ca76000f69c385fe254f246755bbb338298ac1
S Minutoli <sergio.minutoli@gmail.com>
2019-09-27 13:15:51 -0300

    Carrera determina quien gana - fallando

--- posta.test.js
+++ posta.test.js
@@ -10,6 +10,8 @@ function crearCarrera() {
   }
 }
 
+const hipo = { nombre: 'Hipo', velocidad: 13 };
+const astrid = { nombre: 'Astrid', velocidad: 10 };
 describe('una carrera', () => {
   it('admite a cualquier participante', () => {
     // setup
@@ -20,5 +22,16 @@ describe('una carrera', () => {
 
     // expect
     expect(carrera.participantes()).toEqual(postulantes);
+  });
+  it('puede determinar quien gana en base a su velocidad', () => {
+    // setup
+    const carrera = crearCarrera(15);
+    const postulantes = [hipo, astrid];
+    
+    // act
+    carrera.admitir(postulantes);
+
+    // expect
+    expect(carrera.ganador()).toEqual(hipo);
   });
 });
